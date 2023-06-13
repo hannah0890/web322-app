4ab8ef13e1370009ae5e20e0602c91f126177484
thnguyen59 <thnguyen59@myseneca.ca>
2023-06-13 11:16:07 -0400

    adding addItem.html

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 /*********************************************************************************
 *  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy. 
+*  No part of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
 *  Name: __Thi Huyen Nguyen____ Student ID: _129494225____ Date: _June 1 2023____
@@ -13,9 +14,25 @@
 var express = require ("express");
 const path = require ("path");
 const data = require("./store-service");
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
 var app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
+//A3
+cloudinary.config ({
+    cloud_name: 'dsuuvz6hb',
+    api_key: '723662371632512',
+    api_secret: 'Kgua19_OxBtBM03VEddsLVoPI3o',
+    secure: true
+
+})
+
+//A3
+const upload = multer();
+
 
 app.use(express.static('public'));
 
@@ -28,7 +45,7 @@ app.get("/about",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
-
+//shop route
 app.get("/shop",(req,res)=>{
     data.getPublishedItems().then((data)=>{
         res.json(data);
@@ -38,15 +55,58 @@ app.get("/shop",(req,res)=>{
     })
 });
 
-app.get("/items",(req,res)=>{
-    data.getAllItems().then((data)=>{
-        res.json(data);
-    })
-    .catch((error)=>{
-        res.status(500).json({massage:error});
-    })
+//A3
+//set up rout to addItem page
+app.get("/items/add",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/addItem.html"));
 });
 
+//Items route
+app.get("/items",(req,res)=>{
+    if(req.query.category){
+        const category = parseInt(req.query.category);
+        data.getItemsByCategory (category)
+        .then((data)=>{
+            res.json(data);
+        })
+        .catch ((error)=>{
+            res.status(500).json({message: error});
+        });
+    }
+    else if (req.query.minDate){
+        const minDateStr = req.query.minDate;
+        data.getItemsByMinDate(minDateStr)
+        .then((data)=>{
+            res.json(data);
+        })
+        .catch ((error)=>{
+            res.status(500).json({message: error});
+        })
+    }
+    else{
+        data.getAllItems().then((data)=>{
+            res.json(data);
+        })
+        .catch((error)=>{
+            res.status(500).json({massage:error});
+        });
+    }
+    
+});
+
+app.get("/item/:id",(req, res)=>{
+    data.getItemById(req.params.id)
+    .then((data)=>{
+        res.json(data);
+    })
+    .catch((error) => {
+        res.status(500).json({message: error});
+    })
+})
+
+/
+
+//Categories route
 app.get("/categories", (req,res)=>{
     data.getCategories().then((data)=>{
         res.json(data);
@@ -56,6 +116,57 @@ app.get("/categories", (req,res)=>{
     })
 });
 
+//A3
+app.post('items/add', upload.single('featureImage'), (req, res)=>{
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processItem(uploaded.url);
+        });
+    }else{
+        processItem("");
+    }
+     
+    function processItem(imageUrl){
+        req.body.featureImage = imageUrl;
+         
+        // TODO: Process the req.body and add it as a new Item before redirecting to /items
+        const itemData = {
+            featureImage: imageUrl,
+        };
+        data.addItem(itemData).then (()=>{
+            res.redirect("/items");
+        })      
+        .catch((error)=>{
+            res.status(500).send("Unable to add item: " + error);
+        });
+        
+    } 
+    
+});
+ 
+
 
 // 404 error handler
 app.use((req, res) => {
@@ -63,11 +174,8 @@ app.use((req, res) => {
   });
 
 
-
   
   
-
-
 
 //app.listen(HTTP_PORT,onHTTPSTART);
 data.initialize().then(function(){
