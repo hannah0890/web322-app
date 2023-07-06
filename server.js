9c056be30926ac6518e584c845d4c859d9b746c6
thnguyen59 <thnguyen59@myseneca.ca>
2023-07-06 13:38:55 -0400

    update 404

--- server.js
+++ server.js
@@ -310,7 +310,7 @@ app.get("/", (req, res) => {
 
 // 404 error handler
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+    res.render("404");
   });
 
 
